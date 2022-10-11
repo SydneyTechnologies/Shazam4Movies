a804c70eb5fbb0307b18363fc463c72f6df0212b
sydneyIdundun <sydney.idundun@gmail.com>
2022-10-11 07:50:34 +0400

    some changes were made to the static files

--- shazam_movies/api/utils.py
+++ shazam_movies/api/utils.py
@@ -1,6 +1,7 @@
 # this file will contain all the major utility functions that allow this program to function
 
 import os
+from pickle import FRAME
 import time
 from bs4 import BeautifulSoup
 import requests
@@ -11,46 +12,52 @@ CLASSIFIER = "face.xml"
 DEFAULT_WATCH_TIME = 300
 CELEBRITY_URL = "https://starbyface.com/Home/LooksLikeByPhoto"
 faceClassifier = vision.CascadeClassifier(os.path.join(settings.BASE_DIR, f"static/cascades/{CLASSIFIER}"))
+FRAMES = []
 ACTORS_FOUND = {}
 def ReadVideo():
     video = os.path.join(settings.BASE_DIR, "static/test3.MP4")
-    print(video)
     # limit the watch time of the function 
     deadline = time.time() + DEFAULT_WATCH_TIME
     # we first create a capture instance which will read(watch) our video 
     # video location refers to the location of the video in question
     captureInstance = vision.VideoCapture(video)
-    while time.time() < deadline:
-        open, currentFrame = captureInstance.read()
-        # this create a gray scale version of the current frame
-        dim = (int(currentFrame.shape[1] * 2), int(currentFrame.shape[0] * 2))
-        resized = vision.resize(currentFrame, dim, interpolation = vision.INTER_AREA)
-        grayCapture = vision.cvtColor(resized, vision.COLOR_BGR2GRAY)
-        # allows for face detection
-        faces = faceClassifier.detectMultiScale(grayCapture, scaleFactor=1.1, minNeighbors=5)
-        for x, y, w, h in faces:
-            # vision.rectangle(currentFrame, (x-5, y-5), (x + w, y + h), (0, 255, 0), 3)
-            vision.imwrite(os.path.join(settings.BASE_DIR, "static/actors.png"), currentFrame)
-            identifyFace()
+    # captureInstance.set(vision.CAP_PROP_FPS, 60)
+    count = 0
+    if captureInstance.isOpened:
+        while time.time() < deadline:
+            count += 1
+            open, currentFrame = captureInstance.read()
+            if open:
+                if count % 500 == 0:
+                    vision.imwrite(os.path.join(settings.BASE_DIR, f"static/actors/{count}.png"), currentFrame)
+                    FRAMES.append(f"static/actors/{count}.png")
+                if vision.waitKey(1) & 0xFF == ord("q"):
+                    break
+                if count > 1500:
+                    break
+            else:
+                break
         
 
+    identifyFaces()
     print(ACTORS_FOUND)
     captureInstance.release()
-    vision.destroyAllWindows()
-
-def identifyFace():
+    
+def identifyFaces():
     # upload images using pythons request library
-    files = {'imageUploadForm': open(os.path.join(settings.BASE_DIR, "static/actors.png"),'rb')}
-    response= requests.post(CELEBRITY_URL, files=files)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    for frame in FRAMES:
+        print(frame)
+        files = {'imageUploadForm': open(os.path.join(settings.BASE_DIR, frame),'rb')}
+        response= requests.post(CELEBRITY_URL, files=files)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    if response.status_code == 200:
-        result = soup.find("div", attrs= {"class" :"candidate"})
-        if result != None:
-            similarity = result.find(attrs= {"class" :"progress-bar"})['similarity']
-            actor = result.find("p").text
-            if int(similarity) < 40:
-                return 
-            if not ACTORS_FOUND.__contains__(actor):
-                print(f"{actor}: {similarity}")
-                ACTORS_FOUND[actor] = similarity
+        result_div = soup.find_all("div", {"class":"text-center", "id":"result"})[0]
+        print(result_div)
+        if response.status_code == 200:
+            result = soup.find("div", {"class": "realCandidate"})
+            if result != None:
+                similarity = result.find(attrs= {"class" :"progress-bar"})['similarity']
+                actor = result.find("p").text
+                if not ACTORS_FOUND.__contains__(actor):
+                    print(f"{actor}: {similarity}")
+                    ACTORS_FOUND[actor] = similarity
